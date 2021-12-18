6c0ab677770c7e02cf14b992e29f650a8e14976a
Prathyusha <prathyushapeddireddy@Prathyushas-MacBook-Air.local>
2021-12-18 16:08:56 -0500

    intial commit

---
+++ Example3/Models/Film.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Example3.Models
+{
+    public class Film
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
---
+++ Example3/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Example3.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+namespace Example3.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<Film> Films = new List<Film>
+        {
+            new Film { Title = "Naruto", Description = "Hoạt hình", Duration = TimeSpan.FromMinutes(23) },
+            new Film { Title = "Dragon ball", Description = "Hoạt hình", Duration = TimeSpan.FromMinutes(22) },
+            new Film { Title = "Pokemon", Description = "Hoạt hình", Duration = TimeSpan.FromMinutes(21) }
+        };
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public IActionResult OnGetPartial() =>
+                Partial("ProductPartial");
+    }
+}
